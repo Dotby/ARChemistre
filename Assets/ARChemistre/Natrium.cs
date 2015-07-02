c7cd2d761fa005874d45dc35a54b521419e268a7
Pavel <ap@adverine.com>
2015-07-02 19:56:32 +0300

    UPD3

--- Assets/ARChemistre/Natrium.cs
+++ Assets/ARChemistre/Natrium.cs
@@ -7,7 +7,7 @@ public class Natrium : MonoBehaviour {
 	public AudioClip bubble = null;
 	public AudioClip gas = null;
 
-	public ParticleSystem ps; 
+	public ParticleSystem[] ps; 
 	public ParticleSystem smoke;
 
 	AudioSource player;
@@ -17,10 +17,10 @@ public class Natrium : MonoBehaviour {
 	public bool inWater = false;
 
 	public Text infoTxt;
+	public GameObject water;
 
 	// Use this for initialization
 	void Start () {
-
 
 		player = gameObject.AddComponent<AudioSource>();
 
@@ -35,11 +35,17 @@ public class Natrium : MonoBehaviour {
 
 		if (other.gameObject.name == "fluid_cup" && inWater == false){
 			inWater = true;
-			if (tp > 3){
-				StartGas();
+			if (tp > 0){
+				tp -= 0.1f;
+				player.Stop(); 
+				ps[0].Stop(); 
+				ps[1].Stop();
+				PlaySnd(gas);
+				PlaySnd(gas);
 			}
-			else{
-				infoTxt.text = "t = " + tp + "\nТемпература мала для реакции.";
+
+			if (tp > 3){
+				water.GetComponent<Renderer>().sharedMaterial.color = Color.red;
 			}
 		}
 	}
@@ -65,7 +71,7 @@ public class Natrium : MonoBehaviour {
 	public void StartBoil(){
 		PlaySnd(bubble);
 		player.loop = true;
-		ps.Play();
+		ps[0].Play();
 	}
 
 	void StartGas(){
@@ -75,8 +81,18 @@ public class Natrium : MonoBehaviour {
 	}
 
 	void Update () {
+		
+		ps[0].startSpeed = 25.0f - (25.0f - (tp * 10.0f));
+		ps[0].startSize = (100.0f - (100.0f - (tp * 10.0f)))/1.2f;
+		//ps[1].startSpeed = 100.0f - (100.0f - (tp * 10.0f));
 
-		ps.startSpeed = 100.0f - (100.0f - (tp * 10.0f));
+		if (tp > 2 && ps[1].isPlaying == false){
+			ps[1].Play();
+		}
+
+		if (tp < 2 && ps[1].isPlaying == true){
+			ps[1].Stop();
+		}
 
 		if (onFire == true){
 			if (tp < 10.0f){
@@ -94,6 +110,6 @@ public class Natrium : MonoBehaviour {
 		}
 
 		if (tp < 0) {tp = 0;}
-		if (tp == 0) {if (onFire == false) {player.Stop(); ps.Stop();}}
+		if (tp == 0) {if (onFire == false) {player.Stop(); ps[0].Stop(); ps[1].Stop();}}
 	}
 }
